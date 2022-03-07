75573d595133c32a51c5c77cc2c8b77322d22a5b
Maciej Kłonicki <maciek.klonicki@gmail.com>
2022-03-07 22:20:39 +0100

    I added a header with buttons

---
+++ src/components/HeaderComp.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from './Button';
+import './HeaderComp.css';
+
+
+function HeaderComp() {
+    const [click,setClick] = useState(false);
+    const [button, setButton] = useState(true);
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    };
+
+    window.addEventListener('resize', showButton);
+
+  return (
+    <>
+        <nav className='navbar'>
+            <div className='navbar-container'>
+                <div className='menu-icon' onClick={handleClick}>
+                    <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                <li className='nav-item'>
+                    <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                    Home
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                    About me
+                    </Link>
+                </li>
+                <li className='nav-item'>
+                    <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
+                    Applications
+                    </Link>
+                </li>
+                </ul>
+                {button && <Button buttonStyle='btn--outline'>Home</Button>}
+            </div>
+        </nav>
+    </>
+  )
+}
+
+export default HeaderComp
