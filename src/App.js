85b632e282ec102c542b99d8719ab2dd3fe6600c
Maciej Kłonicki <maciek.klonicki@gmail.com>
2022-03-08 20:42:21 +0100

    Changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import HeaderComp from './components/HeaderComp';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 
-
 function App() {
   return (
     <>
