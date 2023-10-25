ff30c9eb6ab95a781dbcbec217e08a6a18ad262d
Maciej Kłonicki <maciej.klonicki@ttpsc.pl>
2023-10-25 13:07:38 +0200

    Added two icons to github and linkedin

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,6 +5,20 @@ function Body() {
     <div className='body-container'>
         <h1>Maciej Kłonicki</h1>
         <h2>Junior Java Developer</h2>
+            <ul class="wrapper">
+                <li class="icon github">
+                <a href="https://github.com/MaciejKlonicki" target="_blank" rel="noopener noreferrer">
+                    <span class="tooltip">Github</span>
+                    <span><i class="fab fa-github"></i></span>
+                </a>
+                </li>
+                <li class="icon linkedin">
+                <a href="https://pl.linkedin.com/in/maciej-k%C5%82onicki" target="_blank" rel="noopener noreferrer">
+                    <span class="tooltip">Linkedin</span>
+                    <span><i class="fab fa-linkedin"></i></span>
+                </a>
+                </li>
+            </ul>
     </div>
   )
 }
