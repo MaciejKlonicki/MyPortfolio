9d8872ae4bd1514dd934917d5d8198ecb4506896
Maciej Kłonicki <maciej.klonicki@ttpsc.pl>
2023-10-31 15:00:04 +0100

    Removed footer, changed LinkedIn text

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Body from './components/Body';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
 import AboutMe from './components/AboutMe';
 import NotFound from './components/NotFound';
 import Projects from './components/Projects';
@@ -17,7 +16,6 @@ function App() {
           <Route path='/projects' component={Projects} />
           <Route path="*" component={NotFound} />
         </Switch>
-        <Footer />
       </Router>
     </>
   );
